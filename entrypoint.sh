ecf87a92456eec2198a71e56d55a5fe06566206f
matthieuEv <evainmatthieu44@gmail.com>
2024-12-30 12:09:54 +0100

    test for samba

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,3 +8,6 @@ sed -i '/%sudo[[:space:]]/ s/ALL[[:space:]]*$/NOPASSWD:ALL/' /etc/sudoers
 
 # Ajouter à lpadmin
 sudo usermod -aG lpadmin $CUPS_USER_NAME
+
+sudo service smbd restart
+sudo service cups restart
